cf97328516ceed7e9f4a1c93b94d9fcd0effb407
Gio <32712976+gptskialadze@users.noreply.github.com>
2024-07-11 16:33:33 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myapp';
+   clients = 
+  [
+    { id: 1, name: "Name1", age: 20, resident: "Y" },
+    { id: 3, name: "Name3", age: 40, resident: "N" },
+    { id: 2, name: "Name2", age: 16, resident: "N" },
+    { id: 5, name: "Name5", age: 25, resident: "Y" },
+    { id: 4, name: "Name4", age: 17, resident: "N" },
+   
+  ]
 }
