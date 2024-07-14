f7ed786a23339f497b3062e14728c1ec685f59a5
NukiKordzaia <akordzaia@bog.ge>
2024-07-14 17:12:43 +0400

    implemented first task.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  cities = [
+  cities: string [] = [
     "Tbilisi", 'Qutaisi', 'Batumi', 'Poti'
   ]
+
+  images: string [] = [
+    "person.jpg", "second.jpg", "third.jfif"
+  ]
+
+  selectedCity: string = "default value";
+  agreeOnTerms: boolean = false;
+  currentImageId: number = 0;
+
+  getImageUrl() {
+    const image = this.images[this.currentImageId]
+    return "../assets/" + image
+  }
+
+  onCitiesDropdownChange(event: any) {
+    this.selectedCity = event.target.value;
+    console.log('selected city is: ' + this.selectedCity);
+  }
+
+  onAgreeOnTerms() {
+    this.agreeOnTerms = !this.agreeOnTerms;
+
+    if (this.agreeOnTerms) {
+      console.log('you agreed on terms, you can Register now!')
+    }
+  }
+
+  changePicture() {
+    this.currentImageId = (this.currentImageId + 1) % this.images.length
+  }
+
+  finish() {
+    window.alert('you finished !')
+  }
 }
