142ae6484bb090e1529a9fb6819e46a45259ffc7
blomi <lomidzeb2@gmail.com>
2024-07-15 10:21:07 +0400

    implement

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
 import { posts } from './posts';
 
 @Component({
@@ -6,7 +6,17 @@ import { posts } from './posts';
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent {
+export class ChildComponent  {
   clientPosts = posts;
+
+  @Input() filter!: string;
+
+  ngOnChanges(changes: SimpleChange) {
+    if (this.filter && this.filter != '') {
+      this.clientPosts = posts.filter((e) => e.title.includes(this.filter));
+    } else {
+      this.clientPosts = posts;
+    }
+  }
 
 }
