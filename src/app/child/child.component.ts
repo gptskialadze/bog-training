ab985774d01dc6b1c563c3c02c20af93a8cb3c5c
mmukhu <mmukh18@freeuni.edu.ge>
2024-07-16 23:18:52 +0400

    final solution

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
 import { posts } from './posts';
 
 @Component({
@@ -8,5 +8,21 @@ import { posts } from './posts';
 })
 export class ChildComponent {
   clientPosts = posts;
+
+  @Input() inputValue!: string;
+
+  ngOnChanges(changes: SimpleChange) {
+    this.clientPosts = [];
+
+    if (this.inputValue.trim() !== '') {
+      for (let i = 0; i < posts.length; i++) {
+        if (posts[i].title.toLowerCase().includes(this.inputValue.toLowerCase().trim())) {
+          this.clientPosts.push(posts[i]);
+        }
+      }
+    } else {
+      this.clientPosts = posts.slice();
+    }
+  }
 
 }
