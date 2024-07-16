214e120aaed2188f12819a3f2e89202099bf25ea
Giorgi Nutsubidze <ginutsubidze@bog.ge>
2024-07-16 22:25:31 +0400

    Feat: Implemented ngOnChanges task

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
-import { posts } from './posts';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {posts} from './posts';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent {
+export class ChildComponent implements OnChanges {
+  @Input() query: string = '';
   clientPosts = posts;
+  filteredPosts = posts;
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['query']) {
+      this.filterPosts();
+    }
+  }
+
+  private filterPosts(): void {
+    this.filteredPosts = this.clientPosts.filter(post =>
+      post.title.toLowerCase().includes(this.query.toLowerCase())
+    );
+  }
 }
