09b8dec7d65c516fc6aa608fa538cab478d2f88c
Mariam Chikovani <chikovanim@bog.ge>
2024-07-17 09:25:19 +0400

    .

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
 import { posts } from './posts';
+import {Component, Input, SimpleChange} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,5 +8,15 @@ import { posts } from './posts';
 })
 export class ChildComponent {
   clientPosts = posts;
+  @Input() title: string = '';
 
+  ngOnChanges(changes: { [propName: string]: SimpleChange }) {
+
+    const filterValue = changes['title'].currentValue;
+
+    console.log('change value: ' + filterValue);
+
+    this.clientPosts = posts.filter(post => post.title.includes(filterValue));
+
+  }
 }
