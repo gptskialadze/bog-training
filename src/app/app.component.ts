f9c56c1a8d080817dd972c566bc27057101b25c1
mmukhu <mmukh18@freeuni.edu.ge>
2024-07-21 19:37:52 +0400

    implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, SimpleChanges} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  isValid: boolean = false;
+  isCopyDisabled: boolean = false;
+  namesMatch: boolean = false;
+  regForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.regForm = this.fb.group({
+      firstName: ['First Name'],
+      lastName: ['Last Name'],
+      email: ['Email Address'],
+      website: ['Website Name'],
+      message: ['Write your message']
+    });
+
+    this.regForm.get('website')?.valueChanges.subscribe((value: string) => {
+      if (value === 'www.myvideo.ge') {
+        const newValue = value + 'my.ge';
+        this.regForm.patchValue({ website: newValue });
+      }
+    });
+  }
+
+  copyAction(): void {
+    this.regForm.setValue({
+      firstName: 'Mariam',
+      lastName: 'Mukhulishvili',
+      email: 'lonelyTylenol961@gmail.com',
+      website: 'bog.ge',
+      message: 'Message Here'
+    })
+  }
+
+  onSubmitCheck(): void {
+    this.isValid = true;
+
+    if (this.regForm.value.firstName?.trim() === '' ||
+      this.regForm.value.lastName?.trim() === '') {
+      this.isValid = false;
+    }
+
+    if (!(this.regForm.value.email?.includes('@'))) {
+      this.isValid = false;
+    }
+
+    // @ts-ignore
+    if (this.regForm.value.website?.length > 10) {
+      this.isValid = false;
+    }
+
+    // @ts-ignore
+    if (!(this.regForm.value.firstName?.at(0).toUpperCase() ===
+      this.regForm.value.firstName?.at(0))) {
+      alert("სახელი უნდა იწყებოდეს მაღალი რეგისტრით");
+      this.isValid = false;
+    }
+
+
+    const currentDate = new Date().toLocaleString();
+    this.regForm.patchValue({
+      message: this.regForm.value.message + currentDate,
+    })
+
+    // @ts-ignore
+    if(this.regForm.value.firstName?.length < 4) {
+      this.isCopyDisabled = true;
+    }
+
+    if(this.regForm.value.firstName === this.regForm.value.lastName) {
+      this.namesMatch = true;
+    }
+  }
+}
+
+interface IRegForm {
+  firstName: FormControl<string>;
+  lastName: FormControl<string>;
+  email: FormControl<string>;
+  website: FormControl<string>;
+  message: FormControl<string>;
 }
