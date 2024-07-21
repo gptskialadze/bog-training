bed279b97832d1e43e3d1e72e90986c28c8ecfdc
NukiKordzaia <akordzaia@bog.ge>
2024-07-21 18:38:19 +0400

    implemented task.

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { posts } from './posts';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {posts} from './posts';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent {
+export class ChildComponent implements OnChanges {
   clientPosts = posts;
 
+  @Input() title: string = '';
+  ngOnChanges(filterWith: SimpleChanges): void {
+    const filterValue :string = filterWith['title'].currentValue;
+    this.clientPosts = posts.filter(post => post.title.includes(filterValue));
+  }
 }
+
