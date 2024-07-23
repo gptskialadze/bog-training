8d9d30f0c1fbb13132e1b55a61191fc45f82aae0
Gio <32712976+gptskialadze@users.noreply.github.com>
2024-07-23 15:04:37 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {data, cities, streets} from './data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  data = data;
+  cities = cities;
+  streets = streets;
 }
