e811482748adea7c5eb2a6ca496b4b38c8017a1b
Giorgi Ptskialadze <gptskialadze@bog.ge>
2024-07-25 12:14:13 +0400

    davaleba routing Vol.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NewsComponent } from './news/news.component';
-import { AboutComponent } from './about/about.component';
-const route: Routes = [
-  {
-    path:'home',
-    component: HomeComponent,
-  },
-  {
-    path:'news',
-    component: NewsComponent,
-  },
-  {
-    path:'about',
-    component: AboutComponent,
-  }
-]
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(route)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
