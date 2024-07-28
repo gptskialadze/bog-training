f2b4c80f105f76a2d1c921468e0431f53a6788ab
ginutsubidze <ginutsubidze@bog.ge>
2024-07-29 01:24:05 +0400

    Feat: implemented 1st step. Streets dropdown according to cities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CityStreetFilterComponent } from './city-street-filter/city-street-filter.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityStreetFilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
