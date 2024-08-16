e5b2ddd11e1d49b87667b0809432aa9e19b67650
Giorgi Ptskialadze <gptskialadze@bog.ge>
2024-08-16 16:12:35 +0400

    'saklaso'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { cities } from './cities';
+import { concat, forkJoin, merge, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,12 @@ import { cities } from './cities';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-   
+   showButton: boolean = false;
 
    ngOnInit() {
     this.initForm();
     this.setStreet();
+    this.changeCheckbox()
    }
 
    myForm!: FormGroup;
@@ -25,14 +27,30 @@ export class AppComponent implements OnInit {
     this.myForm.get("city")?.valueChanges
     .subscribe((value: string) => {
        let street = cities.find((item: any) => item.value == value);
-       this.myForm.get("street")?.setValue(street?.street)
+       this.myForm.get("street")?.setValue(street?.street);
+       this.myForm.get("male")?.setValue(true)
+    })
+  }
+
+  changeCheckbox() {
+    let male: any = this.myForm.get("male")?.valueChanges;
+    let female: any = this.myForm.get("female")?.valueChanges
+    merge(male, female)
+    .subscribe((res) => {
+      if (this.myForm.get("male")?.value || this.myForm.get("female")?.value) {
+        this.showButton = true
+      } else {
+        this.showButton = false
+      }
     })
   }
 
    initForm() {
     this.myForm = new FormGroup({
       city: new FormControl(""),
-      street: new FormControl("")
+      street: new FormControl(""),
+      male: new FormControl(""),
+      female: new FormControl("")
     })
    }
 }
