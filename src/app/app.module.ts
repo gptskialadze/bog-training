f8c3919cef138a95a5402daa540f36ceb679a001
Gio Ptskialadze <gioptskialadze@Gios-MacBook-Pro.local>
2024-08-19 21:10:27 +0400

    saklaso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DirectiveDirective } from './directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DirectiveDirective
+    AppComponent
   ],
   imports: [
     BrowserModule
