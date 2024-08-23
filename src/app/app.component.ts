8564e2bb7ce44ec66e22d944f382206c6942f70b
Gio Ptskialadze <gioptskialadze@Gios-MacBook-Pro.local>
2024-08-23 20:36:59 +0400

    davaleba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Md5 } from 'ts-md5';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent implements OnInit {
+
+  constructor(private service: HttpService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  generateSession() {
+    let md5 = Md5.hashStr("some text");
+    localStorage.setItem("sessionId", md5);
+    
+  }
+
+  getData() {
+    this.service.getPosts().subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
+import { InterceptService } from './intercept.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
