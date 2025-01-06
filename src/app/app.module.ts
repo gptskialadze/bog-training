c8df23210b222cf7caa0e9cf2d9e9c71cb6d12ff
Gio Ptskialadze <gioptskialadze@Gios-MacBook-Pro.local>
2025-01-06 17:31:37 +0400

    pipes sashinao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TranslatorPipe } from './translator.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TranslatorPipe
+    AppComponent
   ],
   imports: [
     BrowserModule
--- src/app/translates.ts
+++
@@ -1,9 +0,0 @@
-export const dictionaryData = [
-    {
-        key: "form.table.title",
-        value: "ცხრილის სახელი"
-    }, {
-        key: "form.table.titles",
-        value: "ცხრილის სახელი"
-    }
-]
