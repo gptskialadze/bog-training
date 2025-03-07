8138a42c65bbf8bc1a330e78a0d7efd086ba2be4
Gio Ptskialadze <gioptskialadze@Gios-MacBook-Pro.local>
2025-03-07 15:38:54 +0400

    dictionary

---
+++ src/app/dictionary.ts
@@ -0,0 +1,22 @@
+export const dictionaries = [
+    {
+        key: 'form.label.username',
+        value: 'User Name'
+    },
+    {
+        key: 'form.label.email',
+        value: 'Email'
+    },
+    {
+        key: 'form.label.acctType',
+        value: 'Acct Type'
+    },
+    {
+        key: 'form.label.cardNo',
+        value: 'Card No'
+    },
+    {
+        key: 'form.button.save',
+        value: 'Save'
+    }
+]
