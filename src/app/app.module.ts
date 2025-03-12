e04a1c2b18f7d0795848e37d8edc9fbd467be0c0
Gio Ptskialadze <gioptskialadze@Gios-MacBook-Pro.local>
2025-03-12 12:50:19 +0400

    routing with params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const route: Routes = [
+   {
+    path: "form",
+    component: FormComponent
+   },
+   {
+    path: "form/:id",
+    component: FormComponent
+   }
+]
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Params } from '@angular/router';
+import { Users } from '../users';
+export interface Parm {
+  id: string
+}
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
 
+
+export class FormComponent {
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  users = Users;
+  user: any;
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const prm  = params as Parm;
+      this.user = this.users.find((elem: any) => elem.id == prm.id)
+    })
+  }
 }
