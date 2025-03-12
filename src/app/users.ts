70c9e4c176741fd354f905a06f656831d44b0e48
Gio Ptskialadze <gioptskialadze@Gios-MacBook-Pro.local>
2025-03-12 11:42:46 +0400

    routing with params

---
+++ src/app/users.ts
@@ -0,0 +1,30 @@
+export const Users = [
+    {
+        id: 1,
+        userName: 'User1',
+        email: 'Email1',
+        acctType: 'A',
+        cardType: 'B'
+    },
+    {
+        id: 2,
+        userName: 'User2',
+        email: 'Email2',
+        acctType: 'AA',
+        cardType: 'BB'
+    },
+    {
+        id: 3,
+        userName: 'User3',
+        email: 'Email3',
+        acctType: 'AAA',
+        cardType: 'BBB'
+    },
+    {
+        id: 4,
+        userName: 'User4',
+        email: 'Email4',
+        acctType: 'AAAA',
+        cardType: 'BBBB'
+    }
+]
