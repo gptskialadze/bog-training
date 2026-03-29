13eed91888a587890e6d7c6b329ff293d5f535bb
Tatulika <tamta.odisharia@gmail.com>
2026-03-29 21:57:13 +0400

    homework10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
- 
+export class AppComponent implements OnInit {
+  data: any[] = [];
+  inputValue: string = '';
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    console.log('1. component initialized, Call getData');
+    this.getData();
+  }
+
+  getData() {
+    console.log('2. Call getData');
+    this.todoService.getTodos().subscribe({
+      next: (e: any) => {
+        if (e) {
+          console.log('3. Data received from service:', e);
+          this.data = Object.values(e);
+          console.log('4. Data after transformation:', this.data);
+        }
+      },
+      error: (err: any) => {
+        console.error('Error occurred:', err);
+      }
+    });
+  }
+
+  onAdd(itemInput: any) {
+    console.log('5. onAdd called with value:', itemInput.value);
+    if (itemInput.value) {
+      this.todoService.addTodo(itemInput.value, new Date()).subscribe({
+        next: () => {
+          console.log('6. Data successfully added to Firebase');
+          itemInput.value = ''; 
+          this.getData(); 
+        },
+        error: (err: any) => {
+          console.error('Add error:', err);
+        }
+      });
+    }
+  }
 }
